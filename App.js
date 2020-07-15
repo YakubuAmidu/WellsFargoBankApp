939a7827f0658a1e8cc6c79fcdadfee9c515b10a
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-15 15:13:07 -0700

    Commiting changes

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+// import { createAppContainer } from 'react-navigation';
+// import { createStackNavigator } from 'react-navigation-stack';
+// import HomeScreen from './src/screens/HomeScreen';
+//
+//
+// const navigator = createStackNavigator({
+//   Home: HomeScreen
+// }, {
+//   initialRouteName: 'Home',
+//   defaultNavigationOptions: {
+//     title: 'SEARCH ENGINE',
+//     style: {
+//       backgroundColor: 'red'
+//     }
+//   }
+// });
+//
+// export default createAppContainer(navigator);
+
+
+import React from 'react';
 import HomeScreen from './src/screens/HomeScreen';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
+const App = () => {
+  return (
+    <ScrollView>
+    <HomeScreen />
+    </ScrollView>
+  );
+};
 
-const navigator = createStackNavigator({
-  Home: HomeScreen
-}, {
-  initialRouteName: 'Home',
-  defaultNavigationOptions: {
-    title: 'SEARCH ENGINE'
+const styles = StyleSheet.create({
+  scrollViewStyle: {
+    
   }
-});
+})
 
-export default createAppContainer(navigator);
+export default App;
