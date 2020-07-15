648a0b3514067506823beaa3cd323704726e1dc6
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-15 15:14:12 -0700

    Commiting changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,91 +1,37 @@
-// import React, { Component } from 'react';
-// import { View, Text, StyleSheet, ScrollView, TextInput } from 'react-native';
-// import { FontAwesome } from '@expo/vector-icons';
-//
-//
-//
-// export default class Google extends Component {
-//   render() {
-//     return (
-//       <ScrollView>
-//       <View>
-//        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
-//        <Text style={{ fontSize: 20, color: 'blue', marginTop: 30, marginBottom: 20, fontSize: 40, fontWeight: 'bold' }}>G</Text>
-//         <Text style={{ fontSize: 20, color: 'red', marginTop: 30, marginBottom: 20, fontSize: 40, fontWeight: 'bold' }}>o</Text>
-//          <Text style={{ fontSize: 20, color: 'gold', marginTop: 30, marginBottom: 20, fontSize: 40, fontWeight: 'bold' }}>o</Text>
-//           <Text style={{ fontSize: 20, color: 'blue', marginTop: 30, marginBottom: 20, fontSize: 40, fontWeight: 'bold' }}>g</Text>
-//            <Text style={{ fontSize: 20, color: 'green', marginTop: 30, marginBottom: 20, fontSize: 40, fontWeight: 'bold' }}>l</Text>
-//             <Text style={{ fontSize: 20, color: 'red', marginTop: 30, marginBottom: 20, fontSize: 40, fontWeight: 'bold' }}>e</Text>
-//             </View>
-//             <View>
-//             <TextInput style={styles.textInputStyle}>
-//             <FontAwesome name="microphone" size={24} color="red" />
-//             </TextInput>
-//             </View>
-//
-//             <View style={styles.viewStyle1}>
-//             <Text style={styles.textStyle2}>Google Search</Text>
-//             <Text style={styles.textStyle2}>I'm Feeling Lucky</Text>
-//             </View>
-//
-//             <View style={styles.viewStyle2}>
-//               <Text style={styles.textStyle3}>We're updating Google.</Text>
-//               <Text style={styles.textStyle4}>Learn more</Text>
-//            </View>
-//
-//               </View>
-//              </ScrollView>
-//            );
-//          };
-//        }
-//
-//        const styles = StyleSheet.create({
-//          textStyle1: {
-//            textAlign: 'center',
-//            fontSize: 50,
-//            marginTop: 20,
-//            color: '#808080'
-//          },
-//          textInputStyle: {
-//            fontSize: 20,
-//            borderWidth: 1,
-//            borderColor: '#ddd',
-//            paddingTop: 10,
-//            paddingBottom: 10,
-//            paddingRight: 10,
-//            marginLeft: 20,
-//            marginRight: 20,
-//            marginTop: 20,
-//            textAlign: 'right',
-//          },
-//          viewStyle1: {
-//            flex: 1,
-//            flexDirection: 'row',
-//            justifyContent: 'center',
-//            marginTop: 20
-//          },
-//          textStyle2: {
-//            backgroundColor: '#DFDFDF',
-//            color: '#808080',
-//            fontWeight: 'bold',
-//            marginRight: 10,
-//            paddingTop: 10,
-//            paddingBottom: 10,
-//            paddingRight: 15,
-//            paddingLeft: 15,
-//            marginBottom: 20
-//          },
-//          textStyle3: {
-//            color: '#808080',
-//            fontWeight: 'bold'
-//          },
-//          viewStyle2: {
-//            flex: 1,
-//            flexDirection: 'row',
-//            justifyContent: 'center'
-//          },
-//          textStyle4: {
-//            color: 'blue',
-//            marginLeft: 5
-//          }
-//        })
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { EvilIcons } from '@expo/vector-icons';
+
+export default class HomeScreen extends Component {
+  render () {
+    return (
+      <View style={styles.ViewStyle}>
+      <AntDesign name="user" size={30} color="white"/>
+       <Text style={styles.textStyle}>Hello world</Text>
+        <EvilIcons name="navicon" size={30} color="white" />
+      </View>
+    );
+  };
+}
+
+const styles = StyleSheet.create({
+  ViewStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+    fontFamily: 'Lato',
+    justifyContent: 'space-between',
+    backgroundColor: 'red',
+    borderBottomWidth: 4,
+    borderColor: 'gold'
+  },
+  textStyle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff',
+    textTransform: 'uppercase'
+  }
+})
