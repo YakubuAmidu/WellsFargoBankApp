6e8dcb9070cdb4103694127efd10de19612f10de
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-15 21:55:43 -0700

    Commiting changes

--- App.js
+++ App.js
@@ -19,21 +19,58 @@
 
 
 import React from 'react';
-import HomeScreen from './src/screens/HomeScreen';
+import TopScreen from './src/screens/TopScreen';
+import PrimaryScreen from './src/screens/PrimaryScreen';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
 
 const App = () => {
   return (
-    <ScrollView>
-    <HomeScreen />
+    <View style={styles.viewStyle1}>
+
+    <View style={styles.viewStyle2}>
+    <EvilIcons name="user" size={35} color="#fff"/>
+    <Text style={styles.textStyle1}>Wells Fargo</Text>
+    <EvilIcons name="navicon" size={35} color="#fff"/>
+    </View>
+
+    <ScrollView style={styles.scrollView}>
+  <PrimaryScreen />
+  <TopScreen />
     </ScrollView>
+    </View>
+
   );
 };
 
 const styles = StyleSheet.create({
-  scrollViewStyle: {
-    
-  }
+  viewStyle1: {
+    borderBottomWidth: 2,
+    borderTopWidth: 2,
+    borderColor: '#ddd'
+  },
+  viewStyle2: {
+   flexDirection: 'row',
+   alignItems: 'center',
+   justifyContent: 'space-between',
+   paddingTop: 10,
+   paddingBottom: 10,
+   paddingLeft: 10,
+   paddingRight: 10,
+   marginBottom: 1,
+   backgroundColor: 'red',
+   borderBottomWidth: 3,
+   borderColor: 'gold'
+ },
+ textStyle1: {
+   fontSize: 30,
+   fontWeight: 'bold',
+   color: '#fff',
+   textTransform: 'uppercase'
+ },
+ scrollView: {
+   width: '100%'
+ }
 })
 
 export default App;
