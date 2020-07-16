24a7628a72b39c0fa2a4bdbbe629436ee31f5e43
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-16 05:49:40 -0700

    Commiting changes

--- src/screens/TopScreen.js
+++ src/screens/TopScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 export default class TopScreen extends Component {
   render () {
@@ -10,6 +11,10 @@ export default class TopScreen extends Component {
          <Text style={styles.textStyle2}> > </Text>
          <Text style={styles.textStyle3}>Interested in managing your spending</Text>
          <Text style={styles.textStyle4}>habits? lets us help</Text>
+         <View style={styles.viewStyle2}>
+         <FontAwesome5 name="money-check-alt" size={24} color="lightblue" />
+         <Text>Manage a dept or fund a purchase</Text>
+         </View>
         </View>
       </View>
     );
@@ -33,5 +38,21 @@ const styles = StyleSheet.create({
     textAlign: 'right',
     fontSize: 20,
     color: '#333'
+  },
+  textStyle3: {
+    textAlign: 'center',
+    color: '#333'
+  },
+  textStyle4: {
+    textAlign: 'center',
+    color: '#333'
+  },
+  viewStyle2: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingLeft: 10,
+    paddingRight: 10
   }
 })
