347771bb1388bd72fb64af5964c87244ca3f45e4
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-16 05:32:12 -0700

    Commiting changes

--- src/screens/TopScreen.js
+++ src/screens/TopScreen.js
@@ -4,9 +4,22 @@ import { View, Text, StyleSheet } from 'react-native';
 export default class TopScreen extends Component {
   render () {
     return (
-      <View>
-      
+      <View style={styles.viewStyle1}>
+       <View>
+        <Text>View Spending Report</Text>
+         <Text> > </Text>
+         <Text>Interested in managing your spending</Text>
+         <Text>habits? lets us help</Text>
+        </View>
       </View>
     );
   };
 }
+
+const styles = StyleSheet.create({
+  viewStyle1: {
+    backgroundColor: '#F0F0F0',
+    marginTop: 2,
+    paddingLeft: 10
+  }
+})
