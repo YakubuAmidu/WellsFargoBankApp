17780a973f960f4bf3a11fa0756eb538b3cad03d
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-16 01:15:55 -0700

    Commiting changes

--- src/screens/PrimaryScreen.js
+++ src/screens/PrimaryScreen.js
@@ -42,13 +42,13 @@ const styles = StyleSheet.create({
   paddingTop: 10,
   paddingBottom: 10,
   marginBottom: 2,
-  backgroundColor: '#D3D3D3'
+  backgroundColor: '#F0F0F0'
 },
   textStyle1: {
     fontSize: 20,
   },
   viewStyle2: {
-    backgroundColor: '#D3D3D3',
+    backgroundColor: '#F0F0F0',
     paddingTop: 15,
     paddingBottom: 15,
     paddingLeft: 10,
@@ -93,7 +93,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    backgroundColor: '#D3D3D3',
+    backgroundColor: '#F0F0F0',
     paddingLeft: 10,
     paddingRight: 10,
     paddingTop: 5,
